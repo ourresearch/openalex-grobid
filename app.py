9b3a416f28f38c740d6b7a7f772840038a382052
Casey M <caseym@gmail.com>
2025-02-21 14:21:26 -0600

    changes

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify
+import os
 
 app = Flask(__name__)
 app.json.sort_keys = False
@@ -6,3 +7,7 @@ app.json.sort_keys = False
 @app.route("/")
 def index():
     return jsonify({"message": "Hello, world!"})
+
+if __name__ == "__main__":
+    debug_mode = os.getenv("FLASK_ENV", "production") == "development"
+    app.run(host="0.0.0.0", port=8080, debug=debug_mode)
